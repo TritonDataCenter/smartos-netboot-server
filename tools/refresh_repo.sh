0ba2ee63df11f6969d89f3eace8b5e2acdfe83f3
Brian Bennett <brian.bennett@mnx.io>
2022-12-08 10:31:25 -0800

    Ensure all data files are readable by nginx

--- tools/refresh_repo.sh
+++ tools/refresh_repo.sh
@@ -8,6 +8,7 @@
 
 #
 # Copyright 2021 Joyent, Inc.
+# Copyright 2022 MNX Cloud, Inc.
 #
 
 # shellcheck disable=SC2154
@@ -27,3 +28,4 @@ data="${1:-./data}"
 "${dirname}/tools/prune_platforms.sh" "$data"
 "${dirname}/tools/render_ipxe_file.sh" "${data}" > "${data}/smartos.ipxe"
 TINKERBELL=1 "${dirname}/tools/render_ipxe_file.sh" "${data}" > "${data}/packet.ipxe"
+chmod ugo+r "$data"
